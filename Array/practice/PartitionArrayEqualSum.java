1eda2a292da4881ebd0dccf44b99c35bfa422012
Utkarsh <vermau2k01@gmail.com>
2024-03-01 04:30:47 +0530

    Partition Array Equal Sum

--- Array/practice/PartitionArrayEqualSum.java
+++ Array/practice/PartitionArrayEqualSum.java
@@ -37,8 +37,6 @@ public class PartitionArrayEqualSum {
         int sum  = 0;
         for(int i = 0;i<arr.length;i++) sum +=arr[i];
 
-        if(sum ==0) return -1;
-
         int sum_so_far = 0;
 
         for(int i = 0;i<arr.length;i++)
